66635d4e7f36c1ce72eb2ebb74b24c86dcdfa5e8
Taylor Farley <twfarley88@gmail.com>
2020-09-29 18:59:00 -0400

    healthy options working

--- client/src/components/meal-list.component.js
+++ client/src/components/meal-list.component.js
@@ -2,32 +2,32 @@ import React, { Component } from "react";
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
 import MealTableRow from './MealTableRow';
-import Health from './health';
+
 
 export default class MealList extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      meals: []
+      meals: [],
+      checkme: false,
+      buttonValue: true
     };
     this.healthyHandler = this.healthyHandler.bind(this)
+  
   }
+
+
+
 
   componentDidMount() {
     axios.get('http://localhost:4000/meals/')
-      .then(res => {
-          //randomize here
+      .then(res => {         
           function shuffle(array) {
             return array.sort(() => Math.random() - 0.5);
-           }
-           
+           }               
            let arr = res.data;           
            let xx = shuffle(arr)
-      
-           //
-          
-          
           this.setState({
           meals: xx       
         });
@@ -35,34 +35,45 @@ export default class MealList extends Component {
       .catch((error) => {
         console.log(error);
       })
-   
-  }
 
+  }
+ 
   DataTable() {
-    // console.log(this.state.meals)
+
     const dates = ['m','t','w','th','f','s','sn']
+
+    if(this.state.checkme)
+    {
     return this.state.meals.map((res, i) => { 
-     
+      if (this.state.checkme==res.healthy)
       return <MealTableRow day={dates[i]} obj={res} key={i} health={JSON.stringify(res.healthy)}/>;
     });
+    }
+    else
+    {
+      return this.state.meals.map((res, i) => { 
+             return <MealTableRow day={dates[i]} obj={res} key={i} health={JSON.stringify(res.healthy)}/>;
+      }); 
+    }
   }
 
-
-  healthyHandler() {
-    const dates = ['m','t','w','th','f','s','sn']
-    return this.state.meals.map((res, i) => { 
-      if(res.healthy)    
-      return <MealTableRow day={dates[i]} obj={res} key={i} health={JSON.stringify(res.healthy)}/>;
-    });
+  healthyHandler(){
+    this.setState(prevState =>{
+      return{
+           ...prevState,
+           checkme : !prevState.checkme,
+           buttonValue: !prevState.buttonValue
+      }
+   })
 }
 
 
 
   render() {
     return (
-    
+
     <div className="table-wrapper">
-     
+ <button onClick={this.healthyHandler}>{this.state.buttonValue? 'Show Me Healthy': 'Show Everything Else'}</button>
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -76,9 +87,9 @@ export default class MealList extends Component {
         <tbody>
           {this.DataTable()}
         </tbody>
-     
+
       </Table>
-     <button onClick={this.healthyHandler}>CLICK ME</button>
+    
     </div>);
   }
 }
